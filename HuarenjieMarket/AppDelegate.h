09d7a866911f53b37ecde1c9dfb1527caa7a3edf
apple <23456>
2016-05-06 14:47:27 +0800

    Initial Commit

---
+++ HuarenjieMarket/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HuarenjieMarket
+//
+//  Created by apple on 16/5/6.
+//  Copyright © 2016年 dujiaqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HuarenjieMarket/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HuarenjieMarket
+//
+//  Created by apple on 16/5/6.
+//  Copyright © 2016年 dujiaqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
